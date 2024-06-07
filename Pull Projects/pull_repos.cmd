36a45a53c9f4a8ccabb64ee69f534cc9b10c53cc
Jarek Szczupak <dante1802@hotmail.com>
2024-06-07 23:56:57 +0200

    cmd

---
+++ Pull Projects/pull_repos.cmd
@@ -0,0 +1,21 @@
+﻿@echo off
+setlocal enabledelayedexpansion
+
+:: Ustawienie zmiennej baseDir
+set "baseDir=C:\Users\dante\Desktop\Istotne\source\Visual Studio\Main"
+
+:: Przechodzenie do folderu głównego
+cd /d "%baseDir%"
+
+:: Pullowanie wszystkich repozytoriów
+for /d %%d in (*) do (
+    if exist "%%d\.git" (
+        echo Pulling changes in %%d
+        pushd %%d
+        git pull
+        popd
+    )
+)
+
+endlocal
+pause
