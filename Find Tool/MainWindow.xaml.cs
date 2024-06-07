e4e783941c6904a46544adaa7e9dd7dc12785c43
Jarek Szczupak <dante1802@hotmail.com>
2024-06-07 22:43:19 +0200

    Refactor: Code cleanup and reformatting across multiple files
    This commit includes non-functional changes across several files. The changes primarily involve the removal of unnecessary `using` directives and reformatting of various elements and braces. Specifically, `App.xaml`, `App.xaml.cs`, `AssemblyInfo.cs`, `CompareFilesHelper.cs`, `MainWindow.xaml.cs`, `ResultWindow.xaml`, and `ResultWindow.xaml.cs` have been cleaned up and reformatted for better readability and maintainability.

--- Find Tool/MainWindow.xaml.cs
+++ Find Tool/MainWindow.xaml.cs
@@ -1,12 +1,7 @@
 ﻿using ConvertUrls.Helpers;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
