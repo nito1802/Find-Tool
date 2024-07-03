2f87cef02b4671cee2c90866732c11292b50e3ab
Jarek Szczupak <dante1802@hotmail.com>
2024-07-03 16:19:48 +0200

    deleteLocalBranch

---
+++ Pull Projects/pull_repos - deleteLocalBranch.cmd
@@ -0,0 +1,28 @@
+﻿@echo off
+setlocal enabledelayedexpansion
+
+:: Ustawienie zmiennej baseDir
+set "baseDir=C:\Users\dante\Desktop\Istotne\source\Visual Studio\Main"
+
+:: Przechodzenie do folderu głównego
+cd /d "%baseDir%"
+
+:: Pullowanie wszystkich repozytoriów
+for /d %%d in (*) do (
+    if exist "%%d\.git" (
+        echo Pulling changes in %%d
+        pushd %%d
+        git pull
+
+        :: Usuwanie lokalnych branży poza "dev" i "master"
+        for /f "tokens=*" %%b in ('git branch ^| findstr /v "dev master"') do (
+            echo Deleting local branch %%b
+            git branch -d %%b
+        )
+
+        popd
+    )
+)
+
+endlocal
+pause
