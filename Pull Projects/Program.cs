6c33259f98d496d6376358bf0c269d8f22c596ea
Jarek Szczupak <dante1802@hotmail.com>
2024-07-19 07:00:13 +0200

    ser os

--- Pull Projects/Program.cs
+++ Pull Projects/Program.cs
@@ -1,65 +1,107 @@
 ﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using System.Xml.Serialization;
+using Newtonsoft.Json.Linq;
 
 namespace Pull_Projects
 {
-    public class IgnoreByteArrayContractResolver : DefaultContractResolver
+    public class CustomerPartConverter : JsonConverter
     {
-        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        public override bool CanConvert(Type objectType)
         {
-            var properties = base.CreateProperties(type, memberSerialization);
-            // Ignoruj właściwości typu byte[]
-            properties = properties.Where(p => p.PropertyType != typeof(byte[])).ToList();
-            return properties;
+            return objectType == typeof(CustomerPart);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var jsonObject = JObject.Load(reader);
+            var customerPart = new CustomerPart();
+
+            // Try to read the single CustomerData object
+            var customerData = jsonObject["CustomerData"]?.ToObject<CustomerData>(serializer);
+            if (customerData != null)
+            {
+                customerPart.Customers = new List<CustomerData>();
+                customerPart.Customers.Add(customerData);
+            }
+
+            // Try to read the list of CustomerData objects
+            var customersData = jsonObject["Customers"]?.ToObject<List<CustomerData>>(serializer);
+            if (customersData != null)
+            {
+                customerPart.Customers = customersData;
+            }
+
+            return customerPart;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var customerPart = (CustomerPart)value;
+
+            writer.WriteStartObject();
+
+            // Write the single CustomerData object for backward compatibility
+            if (customerPart.Customers != null && customerPart.Customers.Count > 0)
+            {
+                writer.WritePropertyName("CustomerData");
+                serializer.Serialize(writer, customerPart.Customers.First());
+            }
+
+            // Write the list of CustomerData objects
+            writer.WritePropertyName("Customers");
+            serializer.Serialize(writer, customerPart.Customers);
+
+            writer.WriteEndObject();
         }
     }
 
-    public class MyModel
+    public class CustomerData
     {
-        public byte[] Content { get; set; }
-        public byte[] Conten2t { get; set; }
         public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+
+        public CustomerData()
+        {
+        }
+    }
+
+    public class CustomerPart
+    {
+        public CustomerPart()
+        {
+        }
+
+        //[JsonIgnore]
+        public List<CustomerData> Customers { get; set; }
+
+        [JsonIgnore]
+        public CustomerData Customer
+        {
+            get
+            {
+                return Customers.First();
+            }
+        }
     }
 
     internal class Program
     {
-        private static void Main()
+        private static void Main(string[] args)
         {
-            var myModel = new MyModel
-            {
-                Name = "Built With Science 5 Minute Daily Stretch Routine.pdf"
-            };
+            var json = "{\"CustomerData\":{\"Name\":\"Jan\",\"Surname\":\"Kowalski\",\"Address\":\"Warszawa\"}}";
 
-            var settings = new JsonSerializerSettings
-            {
-                ContractResolver = new IgnoreByteArrayContractResolver(),
-            };
+            var settings = new JsonSerializerSettings();
+            settings.Converters.Add(new CustomerPartConverter());
 
-            var serialized = JsonConvert.SerializeObject(myModel, settings);
+            var customerPart = JsonConvert.DeserializeObject<CustomerPart>(json, settings);
 
-            string xml = @"
-        <Textbolds>
-            <Textbold>
-                <Width>45in</Width>
-            </Textbold>
-            <Textbold>
-                <Width>2in</Width>
-            </Textbold>
-            <Textbold>
-                <Width>12in</Width>
-            </Textbold>
-        </Textbolds>";
+            var jsonPath = @"C:\Users\dante\Desktop\Istotne\MojeDane\2024\lipiec\19_07_2024\Inne\serialized.json";
+            var serialized = JsonConvert.SerializeObject(customerPart);
+            File.WriteAllText(jsonPath, serialized);
 
-            XmlSerializer serializer = new XmlSerializer(typeof(Textbolds));
-            using (StringReader reader = new StringReader(xml))
-            {
-                Textbolds result = (Textbolds)serializer.Deserialize(reader);
-                foreach (var textbold in result.TextboldList)
-                {
-                    Console.WriteLine(textbold.Width);
-                }
-            }
+            var customerPartNew = JsonConvert.DeserializeObject<CustomerPart>(json, settings);
+
+            Console.WriteLine("Hello, World!");
         }
     }
 }
