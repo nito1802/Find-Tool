8f12a0d224097b22be5b4a1536ad287d785f730e
Jarek Szczupak <dante1802@hotmail.com>
2024-12-24 05:46:59 +0100

    sql

---
+++ Pull Projects/Class1.sql
@@ -0,0 +1,11 @@
+﻿
+	SELECT 
+    CAST(
+        DATEADD(HOUR, 
+            DATEPART(HOUR, MojaData AT TIME ZONE 'Central European Standard Time') 
+            - DATEPART(HOUR, SWITCHOFFSET(MojaData AT TIME ZONE 'Central European Standard Time', 0)), 
+            MojaData AT TIME ZONE 'Central European Standard Time'
+        ) AS datetime
+    ) AS AdjustedTime
+FROM 
+    [MyDatabase].[dbo].[Orders];
