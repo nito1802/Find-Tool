0ed11b5bdd5d00a137a18a116be710c411b256a6
Jarek Szczupak <dante1802@hotmail.com>
2025-02-04 10:34:45 +0100

    ext

---
+++ Pull Projects/ListExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pull_Projects
+{
+    public static class ListExtensions
+    {
+        public static List<T> ShiftToFirst<T>(this List<T> list, Func<T, bool> predicate)
+        {
+            if (list == null || list.Count < 2)
+                return list;
+
+            var targetItem = list.SingleOrDefault(predicate);
+            if (targetItem == null)
+                return list;
+
+            return new List<T> { targetItem }
+                .Concat(list.Where(item => !predicate(item)))
+                .ToList();
+        }
+    }
+}
