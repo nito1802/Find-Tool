99e47241cce94e537ecc1876332e0dc1173ab956
Jarek Szczupak <dante1802@hotmail.com>
2025-05-13 08:08:59 +0200

    attr

---
+++ Pull Projects/Attribut.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.FeatureManagement;
+using System.Threading.Tasks;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+public class FeaturePolicyAuthorizeAttribute : Attribute, IAsyncAuthorizationFilter
+{
+    private readonly string _policy;
+    private readonly string _featureFlag;
+
+    public FeaturePolicyAuthorizeAttribute(string policy, string featureFlag)
+    {
+        _policy = policy;
+        _featureFlag = featureFlag;
+    }
+
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        var featureManager = context.HttpContext.RequestServices.GetService(typeof(IFeatureManager)) as IFeatureManager;
+        var authService = context.HttpContext.RequestServices.GetService(typeof(IAuthorizationService)) as IAuthorizationService;
+        var user = context.HttpContext.User;
+
+        if (featureManager == null || authService == null)
+        {
+            context.Result = new ForbidResult();
+            return;
+        }
+
+        var isFeatureEnabled = await featureManager.IsEnabledAsync(_featureFlag);
+        if (!isFeatureEnabled)
+        {
+            context.Result = new NotFoundResult(); // lub ForbidResult jeśli wolisz
+            return;
+        }
+
+        var authResult = await authService.AuthorizeAsync(user, _policy);
+        if (!authResult.Succeeded)
+        {
+            context.Result = new ForbidResult();
+        }
+    }
+}
+
+[FeaturePolicyAuthorize("AdminOnly", "MyCoolFeature")]
+public IActionResult SecretStuff()
+{
+    return Ok("You got in!");
+}
